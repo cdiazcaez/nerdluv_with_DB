b6d2ccd84a51fea0c5e2dfb43018923b15040bc8
Christian <chris_1693@yahoo.com>
2019-04-04 19:31:44 -0400

    finishing signup.php
    adding fields for Personlity type, Favorite OS, and seeling age.
    
    input type submit to send data form to the action"script"

--- signup.php
+++ signup.php
@@ -20,3 +20,33 @@
 			<strong>Age:</strong>
 			<input type="number" name="age" size="5" min="1" max="99" />
 		</div>
+
+
+		<div>
+			<strong>Personality type</strong>
+			<input type="text" name="type" size="5" maxlength="4" />
+			(<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>): <br />
+		</div>
+
+		<div>
+			<strong>Favorite OS:</strong>
+			<select name="os">
+				<option value="Windows">Windows</option>
+				<option value="Mac OS X">Mac OS X</option>
+				<option value="Linux">Linux</option>
+				<option value="other">other</option>
+			</select>
+		</div>
+
+		<div>
+			<strong>Seeling age:</strong>
+			<input type="number" name="min" placeholder="min" size="5" min="1" max="99" /> and
+			<input type="number" name="max" placeholder="max" size="5" min="1" max="99" />
+		</div>
+
+		<input type="submit" value="Sign Up" />
+	</fieldset>
+</form>
+
+<?php include("bottom.html"); ?>
+
