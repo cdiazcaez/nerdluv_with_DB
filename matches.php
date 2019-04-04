e706e18913756e927c9de7a235233826fd4b9b95
Christian <chris_1693@yahoo.com>
2019-04-04 18:11:57 -0400

    writting matches.php
    form action, on submit  it send the form data to the matches-submit.php

---
+++ matches.php
@@ -0,0 +1,21 @@
+<?php include("top.html"); ?>
+
+
+<form action="matches-submit.php" method="GET">
+
+	<fieldset>
+		
+		<div>
+			Name: <br />
+			<input type="text" name="name" size="16" />
+		</div>
+
+		<div>
+			<!-- View Matches button -->
+		</div>
+
+		<input type="submit" value="View My Matches" />
+	</fieldset>
+</form>
+
+<?php include("bottom.html"); ?>
