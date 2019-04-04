1c32bf5ba280197145e4b08fa7208ea4385f49f9
Christian <chris_1693@yahoo.com>
2019-04-04 19:16:11 -0400

    continue submit matches
    adding top.html
    
    statement if NOT Match, said message not found.
    
    using foreach to find matches (name, gender, type, os)

--- matches-submit.php
+++ matches-submit.php
@@ -9,3 +9,37 @@
 	$matches = $user->match($name);
 
 ?>
+
+
+<?php include("top.html"); ?>
+
+
+<h1>Matches for <?=$name;?></h1>
+<?php
+	
+	if(empty($matches)) { ?>
+		<p>No match is found.</p>
+
+	<?php } else {
+
+		foreach($matches as $match) { ?>
+
+			<div class="match">
+				<p>
+					<img src="public/user.jpg" alt="avatar" />
+					<?=$match["name"];?>
+				</p>
+				<ul>
+					<li>gender: <?=$match["gender"];?></li>
+					<li>age: <?=$match["age"];?></li>
+					<li>type: <?=$match["type"];?></li>
+					<li>OS: <?=$match["os"];?></li>
+				</ul>	
+
+			</div>
+
+		<?php } 
+	} ?>
+
+
+<?php include("bottom.html"); ?>
