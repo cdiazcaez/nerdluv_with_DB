ce50536adb783ee71aee045cbe4ad37d9390bc3d
Christian <chris_1693@yahoo.com>
2019-04-04 21:37:46 -0400

    inserting daa into database
    ###########
    PDO::beginTransaction — Initiates a transaction
    Reference
    https://www.php.net/manual/en/pdo.begintransaction.php
    #############
    prepare statements: used to execute the same (or similar) SQL statements repeatedly with high efficiency.
    Reference
    https://www.w3schools.com/php/php_mysql_prepared_statements.asp
    ##################
    PDOStatement::execute — Executes a prepared statement

--- classes/user.php
+++ classes/user.php
@@ -13,3 +13,29 @@ class User {
 
 
 	public function add($user) {
+
+		try {
+
+			$this->conn->beginTransaction();
+
+			$stmt = $this->conn->prepare("INSERT INTO users (name, gender, age) VALUES (:name, :gender, :age)");
+			$stmt->execute([
+				$user["name"],
+				$user["gender"],
+				$user["age"]
+			]);
+
+			$stmt = $this->conn->prepare("SET @userId = LAST_INSERT_ID();");
+			$stmt->execute();
+
+			$stmt = $this->conn->prepare("INSERT INTO user_personality (user, type) VALUES (@userId, :type)");
+			$stmt->execute([ $user["type"] ]);
+
+			$stmt = $this->conn->prepare("INSERT INTO user_os (user, name) VALUES (@userId, :os)");
+			$stmt->execute([ $user["os"] ]);
+
+			$stmt = $this->conn->prepare("INSERT INTO user_seeking_age (user, smin, smax) VALUES (@userId, :min, :max)");
+			$stmt->execute([ $user["min"], $user["max"] ]);
+
+
+		    return $this->conn->commit();
